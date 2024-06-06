a76ce08420bf7605c59fb4009f6facc7a4a67b89
kuma <kuma@sonicgarden.jp>
2024-06-06 22:07:50 +0900

    fix: tailwindがhamlでも動くように

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import daisyui from "daisyui"
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
+    './app/views/**/*.html.haml',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
