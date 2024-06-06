5bb9539bc2f1869e20ed5bce98c738f565b745b9
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-06-06 21:12:10 +0900

    add daisyui package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+import daisyui from "daisyui"
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  plugins: [
+      daisyui,
+  ],
 }
