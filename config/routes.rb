4f1318b3443513f191332a4d241786469be5e08d
kuma <kuma@sonicgarden.jp>
2024-06-07 10:43:34 +0900

    feat: google認証できるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'log_out', to: 'sessions#destroy', as: 'log_out'
   root 'home#index'
 end
