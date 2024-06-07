08e691a2262a402dc58592961f8fb11d6ec838db
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-06-07 16:02:37 +0900

    見た目を整えた

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import daisyui from "daisyui"
 module.exports = {
   content: [
     './app/views/**/*.html.haml',
+    './app/views/**/*.turbo_stream.haml',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
